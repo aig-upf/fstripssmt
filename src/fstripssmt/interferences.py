559f99b3a4594f2742ccc9d6be1f02f7cc105165
Joan Espasa Arxer <cat.os.mandros@gmail.com>
2020-05-27 16:20:41 +0100

    fixed a couple of things

--- src/fstripssmt/interferences.py
+++ src/fstripssmt/interferences.py
@@ -2,13 +2,18 @@ import copy
 import sys
 
 from multipledispatch import dispatch
-
+from pysmt.shortcuts import FreshSymbol, Symbol, EqualsOrIff, Int, Real, FunctionType, And, ForAll, Exists, get_env
+from pysmt.shortcuts import LT, GE, Equals, Implies, Or, TRUE, FALSE, Not, Symbol
+from pysmt.typing import INT, BOOL, REAL
+from tarski.syntax.ops import all_variables
 #from fstripssmt.runner import decode_smt_model
+from tarski.syntax.ops import compute_sort_id_assignment
 from fstripssmt.solvers.common import solve
 from .solvers.pysmt import PySMTTranslator, print_as_smtlib
 from .errors import TransformationError
 
 import tarski
+from tarski import Term
 from tarski.syntax.ops import free_variables
 from tarski.utils import resources
 from tarski.syntax import top
@@ -36,6 +41,7 @@ class SemanticInterferences(ProblemWalker):
         self.static_symbols = static_symbols
         self.metalang = self.setup_metalang(problem)
         _, self.static_symbols = approximate_symbol_fluency(problem)
+        self.sort_bounds, self.object_ids = compute_sort_id_assignment(self.metalang)
 
     def setup_metalang(self, problem):
         """ Set up the Tarski metalanguage where we will build the SMT compilation. """
@@ -50,9 +56,8 @@ class SemanticInterferences(ProblemWalker):
                 else:
                     ml.sort(s.name, parent(s).name)
 
-        # Declare an extra "timestep" sort. Note: ATM Just using unbounded Natural objects
-        # ml.Timestep = ml.interval("timestep", _get_timestep_sort(ml), 0, 5)
-        # ml.Timestep = ml.interval("timestep", ml.Real, 0, 5)
+        # Declare an extra "timestep" sort with a large range, which we'll adjust once we know the horizon
+        ml.Timestep = ml.interval("timestep", ml.Natural, 0, 99999)
 
         # Declare all objects in the metalanguage
         for o in lang.constants():
@@ -76,7 +81,8 @@ class SemanticInterferences(ProblemWalker):
         return ml
 
     def to_metalang(self, phi, t, subt=None):
-        """ parameters:
+        """ Transform a formula phi to a formula suitable for being translated to SMT directly
+            parameters:
                 - phi: formula
                 - t : timestep variable
                 - subt : shall we substitute the timestep for something?
@@ -136,32 +142,55 @@ class SemanticInterferences(ProblemWalker):
         if not(sigma_a = sigma_b) is T-satisfiable
         """
         ml = self.metalang
-        # Don't use the timestep arg
-        vars_ = generate_action_arguments(ml, a, char='a') + generate_action_arguments(ml, b, char='b')
-        vart = _get_timestep_var(ml)
-        args = vars_ + [vart]
-        print(args)
 
+        # Must check for each pair of effects in case they talk about the same predicate
         for a_eff in a.effects:
             for b_eff in b.effects:
-                print(f"lets check if effects \n\t action a:{a} --- effect:{a_eff} \n\t action b:{b} --- effect:{b_eff} \n are simply commuting:")
-                # substitute the parameters on the effects
-                vars_a, translated_a = self.get_translated_effect(a, a_eff, 'a_')
-                vars_b, translated_b = self.get_translated_effect(b, b_eff, 'b_')
+                modified_a = _get_affected_atom(a_eff)
+                modified_b = _get_affected_atom(b_eff)
+                if modified_a.symbol == modified_b.symbol:
 
-                # TODO: substitution one on another
+                    print(f"as both talk about {modified_a.symbol}, lets check if they are simply commuting:")
+                    print(f"\t action a:{a} --- effect:{a_eff} \n\t action b:{b} --- effect:{b_eff})")
 
-                # lets check if not (sigma_a = sigma_b) is T - satisfiable
-                # construct the problem and ask the SMT solver if SAT, break, else continue searching
-                total_args = vars_a + vars_b + [_get_timestep_var(ml)]
-                theory = [forall(*total_args, neg(equiv(translated_a,translated_b)))]
+                    # compute the substitutions
+                    substitution = {}
+                    self.effect_as_substitution(a_eff, substitution)
+                    self.effect_as_substitution(b_eff, substitution)
 
-                model = self.solve_theory(theory)
-                if model:
-                    print("SAT, this means that they are NOT simply commuting")
-                    print(model)
-                else:
-                    print("UNSAT, this means that they are simply commuting")
+                    # substitute the parameters on the effects
+                    vars_a, subs_a, translated_a = self.get_translated_effect(a, a_eff, 'a_')
+                    vars_b, subs_b, translated_b = self.get_translated_effect(b, b_eff, 'b_')
+
+                    # and make the substitutions: Eff_a \sigma_b, and Eff_b \sigma_a
+                    a_seff = term_substitution(translated_a, substitution)
+                    b_seff = term_substitution(translated_b, substitution)
+
+                    # ------- Some debug statements -------
+                    print(substitution)
+                    print(f"substituting {translated_a} into {translated_b} lends to: {a_seff}")
+                    print(f"substituting {translated_b} into {translated_a} lends to: {b_seff}")
+
+                    # construct not (sigma_a = sigma_b)
+                    neq_eff = neg(equiv(translated_a,translated_b))
+
+                    # we must force that parameters are the same to be able to say that we are talking
+                    # about the same underlying variable (i.e. the same grounded var)
+                    parameters_term_a = term_substitution(modified_a, subs_a).subterms
+                    parameters_term_b = term_substitution(modified_b, subs_b).subterms
+                    equalities = [ a == b for a, b in zip(parameters_term_a, parameters_term_b)]
+
+                    # finally lets check if not (sigma_a = sigma_b) is T - satisfiable
+                    # construct the problem and ask the SMT solver if SAT, break, else continue searching
+                    vart = _get_timestep_var(ml)
+                    all_vars = vars_a + vars_b + [vart]
+                    final_formula = exists(*all_vars, land(*(equalities + [neq_eff])))
+                    model = self.solve_theory([ final_formula ], ml)
+                    if model:
+                        print("SAT, this means that they are NOT simply commuting")
+                        return True
+                    else:
+                        print("UNSAT, this means that they are simply commuting")
         return True
 
 
@@ -193,7 +222,7 @@ class SemanticInterferences(ProblemWalker):
         else:
             raise TransformationError(f"Can't compile effect {eff}")
         # finally return the encoded effect
-        return vars_, trans_eff
+        return vars_, substitution_, trans_eff
 
     def check1(self, a, b):
         """
@@ -217,7 +246,7 @@ class SemanticInterferences(ProblemWalker):
         """ returns True if a given symbol is a fluent, i.e. it can change over time steps """
         return not symbol.builtin and symbol not in self.static_symbols
 
-    def solve_theory(self, theory):
+    def solve_theory(self, theory, language):
         """ given a theory, solve it. TODO: translator """
         # Some sanity check: All formulas must be sentences!
         for formula in theory:
@@ -229,13 +258,53 @@ class SemanticInterferences(ProblemWalker):
         horizon = 2 # we only check t and t+1, i.e. one transition
         with resources.timing(f"Translating and solving", newline=True):
             anames = set(a.name for a in self.problem.actions.values())
-            translator = PySMTTranslator(self.metalang, self.static_symbols, anames)
+            translator = PySMTTranslator(language, self.static_symbols, anames)
+            #print(f"theory: {theory}")
             translated = translator.translate(theory, horizon)
+            #print(f"translated: {translated}")
             # Let's simplify the sentences for further clarity
             translated = [t.simplify() for t in translated]
             print_as_smtlib(translated,[], sys.stdout)
             model = solve(translated, 'z3')
             return model #decode_smt_model(model, horizon, translator)
+
+    def effect_as_substitution(self, eff, substitution={}):
+        # An add effect means identity substitution
+        vart = _get_timestep_var(self.metalang)
+        if isinstance(eff, fs.AddEffect):
+            x_t = self.to_metalang(eff.atom, vart, subt=vart)
+            substitution[symref(x_t)] = x_t
+        elif isinstance(eff, fs.DelEffect):
+            x_t = self.to_metalang(eff.atom, vart, subt=vart)
+            substitution[symref(x_t)] = self.to_metalang(neg(x_t), vart, subt=vart)
+        elif isinstance(eff, fs.FunctionalEffect):
+            x_t = self.to_metalang(eff.lhs, vart, subt=vart)
+            substitution[symref(x_t)] = self.to_metalang(eff.rhs, vart, subt=vart)
+        else:
+            print(f"What is {eff}? Baby don't hurt me!")
+
+    def get_expression_bounds(self, expr):
+        s = expr.sort
+        # Note that bounds in Tarski intervals are inclusive, while here we expect an exclusive upper bound
+        return (s.lower_bound, s.upper_bound + 1) if isinstance(s, Interval) else self.sort_bounds[s]
+
+    def create_quantified_variable(self, v, lang):
+        # First deal with the two unbound cases:
+        if v.sort == lang.Integer:
+            return Symbol(v.symbol, INT), TRUE()
+
+        if v.sort == lang.Real:
+            return Symbol(v.symbol, REAL), TRUE()
+
+        # Otherwise assume we have a bounded type (including Enumerated types)
+        smtvar = Symbol(v.symbol, INT)
+
+        lb, up = self.get_expression_bounds(v)
+        if lb >= up:
+            raise TransformationError(f"SMT variable corresponding to sort '{v.sort}' has cardinality 0")
+
+        bounds = And(GE(smtvar, Int(lb)), LT(smtvar, Int(up)))
+        return smtvar, bounds
 
 # auxiliary functions stolen from the lifted encoding file
 def _get_timestep_sort(lang):
@@ -253,3 +322,9 @@ def _get_timestep_var(lang, name="t"):
 
 def _get_timestep_const(lang, value):
     return Constant(value, _get_timestep_sort(lang)) if isinstance(value, int) else value
+
+def _get_affected_atom(eff):
+    """ Given an effect, it returns the predicate that is changed """
+    return eff.atom if isinstance(eff, (fs.AddEffect, fs.DelEffect)) else eff.lhs
+
+
